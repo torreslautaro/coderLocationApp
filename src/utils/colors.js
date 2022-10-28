1c269a0a06855ad37974b074fc983e9a55b22528
Lautaro Torres <lautaromt.94@gmail.com>
2022-10-28 20:42:10 -0300

    Subo proyecto para entregable 16 y 17

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#D44D5C",
+  secondary: "#773344",
+  tertiary: "#D3D2C7",
+  background: "#D3C2CE",
+  text: "#212121",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+};
